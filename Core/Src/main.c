2288cd2e3b5e9d9f082b8745bad1ab147850794e
yusicheng <2952522041@qq.com>
2026-07-29 11:34:33 +0800

    配置循迹uart引脚,添加tim4作为100hz中断,添加i2c总线适用于mpu6050和oled

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -91,6 +93,9 @@ int main(void)
   MX_TIM1_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
   Motor_Init();
 
